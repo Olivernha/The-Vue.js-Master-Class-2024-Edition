e92312c33bafeb7771d6b5a25158fc65d5d5abf0
Nay_Htet_Aung <nayhtetaung201733@gmail.com>
2025-04-29 23:35:15 +0800

    feat: setup auto route with typescript

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,9 @@
 
-import { h } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: () => import('@/views/HomeView.vue')
-    },
-    {
-      path:'/projects',
-      name: 'Projects',
-      component: () => import('@/views/ProjectsView.vue')
-    },
-    {
-      // nowhre/nowhere
-      path:'/:catchAll(.*)*',
-      name: 'NotFound',
-      component: h('p',{style : {color:'red'}},'404 Not Found')
-    }
-  ],
+  routes,
 })
 
 export default router
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import VueRouter from 'unplugin-vue-router/vite'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -8,6 +9,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig({
   plugins: [
     vue(),
+    VueRouter(),
     vueDevTools(),
   ],
   resolve: {
