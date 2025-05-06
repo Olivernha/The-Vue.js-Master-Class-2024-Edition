8e58bc88892f60cd950f631e799fb89c9e23c1e9
Nay_Htet_Aung <nayhtetaung201733@gmail.com>
2025-05-06 17:27:40 +0800

    feat:apply typescript

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,8 +1,27 @@
 <script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+import { ref } from 'vue'
+
+const projects = ref<any[] | null>(null)
+;(async () => {
+  const { data, error } = await supabase.from('projects').select()
+
+  if (error) console.log(error)
+
+  projects.value = data
+
+  console.log('projects: ', projects.value)
+})()
 </script>
+
 <template>
   <div>
-    <h1>Welcome to the Projects Page</h1>
-    <p>This is a simple projects page built with Vue 3 and TypeScript.</p>
+    <h1>Projects Page</h1>
+    <RouterLink to="/">Go to home</RouterLink>
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        {{ project }}
+      </li>
+    </ul>
   </div>
 </template>
