020cd5dee6f634f36af7e6688b1d746ffd2e176b
Nay_Htet_Aung <nayhtetaung201733@gmail.com>
2025-05-08 11:26:37 +0800

    feat:  extract supaqueries

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -1,22 +1,10 @@
 <script setup lang="ts">
-import { supabase } from '@/lib/supabaseClient'
-
-import type { ColumnDef } from '@tanstack/vue-table'
 import { RouterLink } from 'vue-router'
-import type { QueryData } from '@supabase/supabase-js'
+import { tasksWithProjectsQuery } from '@/utils/supaQueries'
+import type { ColumnDef } from '@tanstack/vue-table'
+import type { TasksWithProjects } from '@/utils/supaQueries'
 
 usePageStore().pageData.title = 'My Tasks'
-
-const tasksWithProjectsQuery = supabase.from('tasks').select(`
-    *,
-    projects (
-      id,
-      name,
-      slug
-    )
-  `)
-
-type TasksWithProjects = QueryData<typeof tasksWithProjectsQuery>
 
 const tasks = ref<TasksWithProjects | null>(null)
 const getTasks = async () => {
@@ -38,25 +26,25 @@ const columns: ColumnDef<TasksWithProjects[0]>[] = [
         RouterLink,
         {
           to: `/tasks/${row.original.id}`,
-          class: 'text-left font-medium hover:bg-muted block w-full',
+          class: 'text-left font-medium hover:bg-muted block w-full'
         },
-        () => row.getValue('name'),
+        () => row.getValue('name')
       )
-    },
+    }
   },
   {
     accessorKey: 'status',
     header: () => h('div', { class: 'text-left' }, 'Status'),
     cell: ({ row }) => {
       return h('div', { class: 'text-left font-medium' }, row.getValue('status'))
-    },
+    }
   },
   {
     accessorKey: 'due_date',
     header: () => h('div', { class: 'text-left' }, 'Due Date'),
     cell: ({ row }) => {
       return h('div', { class: 'text-left font-medium' }, row.getValue('due_date'))
-    },
+    }
   },
   {
     accessorKey: 'projects',
@@ -67,12 +55,12 @@ const columns: ColumnDef<TasksWithProjects[0]>[] = [
             RouterLink,
             {
               to: `/projects/${row.original.projects.slug}`,
-              class: 'text-left font-medium hover:bg-muted block w-full',
+              class: 'text-left font-medium hover:bg-muted block w-full'
             },
-            () => row.original.projects?.name,
+            () => row.original.projects?.name
           )
         : ''
-    },
+    }
   },
   {
     accessorKey: 'collaborators',
@@ -81,10 +69,10 @@ const columns: ColumnDef<TasksWithProjects[0]>[] = [
       return h(
         'div',
         { class: 'text-left font-medium' },
-        JSON.stringify(row.getValue('collaborators')),
+        JSON.stringify(row.getValue('collaborators'))
       )
-    },
-  },
+    }
+  }
 ]
 </script>
 
