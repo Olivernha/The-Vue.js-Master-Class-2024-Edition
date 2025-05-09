396ae1f8903acc4b024362bfb28c72059f9b2156
Nay Htet Aung <nayha@tuaspower.com.sg>
2025-05-09 17:01:16 +0800

    feat: create new AppErrorProdSection

--- src/components/AppError/AppErrorPage.vue
+++ src/components/AppError/AppErrorPage.vue
@@ -1,87 +1,76 @@
 <script setup lang="ts">
-      import { computed } from 'vue'
-      import { useRouter } from 'vue-router'
-      import { useErrorStore } from '@/stores/error'
+const router = useRouter()
 
-      const router = useRouter()
-      const errorStore = useErrorStore()
+const errorStore = useErrorStore()
 
-      const error = computed(() => errorStore.activeError)
+const error = ref(errorStore.activeError)
 
-      const errorData = computed(() => {
-        if (!error.value) return {}
+const message = ref('')
+const customCode = ref(0)
+const details = ref('')
+const code = ref('')
+const hint = ref('')
+const statusCode = ref(0)
 
-        if (!('code' in error.value)) {
-          return {
-            message: error.value.message,
-            customCode: error.value.customCode ?? 0,
-            details: '',
-            hint: '',
-            code: '',
-            statusCode: 0,
-          }
-        }
+if (error.value && !('code' in error.value)) {
+  message.value = error.value.message
+  customCode.value = error.value.customCode ?? 0
+}
 
-        return {
-          message: error.value.message,
-          customCode: 0,
-          details: error.value.details,
-          hint: error.value.hint,
-          code: error.value.code,
-          statusCode: error.value.statusCode ?? 0,
-        }
-      })
+if (error.value && 'code' in error.value) {
+  message.value = error.value.message
+  details.value = error.value.details
+  hint.value = error.value.hint
+  code.value = error.value.code
+  statusCode.value = error.value.statusCode ?? 0
+}
 
-      router.afterEach(() => {
-        errorStore.activeError = null
-      })
-      </script>
+router.afterEach(() => {
+  errorStore.activeError = null
+})
+</script>
 
-      <template>
-        <section class="error">
-          <div>
-            <iconify-icon icon="lucide:triangle-alert" class="error__icon" />
-            <h1 class="error__code">{{ errorData.customCode || errorData.code }}</h1>
-            <p class="error__code" v-if="errorData.statusCode">Status Code: {{ errorData.statusCode }}</p>
-            <p class="error__msg">{{ errorData.message }}</p>
-            <p v-if="errorData.hint">{{ errorData.hint }}</p>
-            <p v-if="errorData.details">{{ errorData.details }}</p>
-            <div class="error-footer">
-              <p class="error-footer__text">You'll find lots to explore on the home page.</p>
-              <RouterLink to="/">
-                <Button class="max-w-36">Back to homepage</Button>
-              </RouterLink>
-            </div>
-          </div>
-        </section>
-      </template>
+<template>
+  <section class="error">
+    <AppErrorDevSection :message :customCode :code :statusCode :hint :details />
+    <!-- <AppErrorProdSection
+      :message
+      :customCode
+      :code
+      :statusCode
+      :hint
+      :details
+      :isCustomError="errorStore.isCustomError" -->
+    />
+  </section>
+</template>
 
-      <style scoped>
-      .error {
-        @apply mx-auto flex justify-center items-center flex-1 p-10 text-center -mt-20 min-h-[90vh];
-      }
+<style scoped>
+.error {
+  @apply mx-auto flex justify-center items-center flex-1 p-10 text-center -mt-20 min-h-[90vh];
+}
 
-      .error__icon {
-        @apply text-7xl text-destructive;
-      }
+:deep(.error__icon) {
+  @apply text-7xl text-destructive;
+}
 
-      .error__code {
-        @apply font-extrabold text-7xl text-secondary;
-      }
+:deep(.error__code) {
+  @apply font-extrabold text-7xl text-secondary;
+}
 
-      .error__msg {
-        @apply text-3xl font-extrabold text-primary;
-      }
+:deep(.error__msg) {
+  @apply text-3xl font-extrabold text-primary;
+}
 
-      .error-footer {
-        @apply flex flex-col items-center justify-center gap-5 mt-6 font-light;
-      }
+:deep(.error-footer) {
+  @apply flex flex-col items-center justify-center gap-5 mt-6 font-light;
+}
 
-      .error-footer__text {
-        @apply text-lg text-muted-foreground;
-      }
+:deep(.error-footer__text) {
+  @apply text-lg text-muted-foreground;
+}
 
-      p {
-        @apply my-2;
-      }
-      </style>
+:deep(p) {
+  @apply my-2;
+}
+</style>
