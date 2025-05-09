13cb86d71cc9810826dfeb282ba17f0362c098e8
Nay Htet Aung <nayha@tuaspower.com.sg>
2025-05-09 16:15:17 +0800

    feat: add onErrorCaptured Hook

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
-import { useErrorStore } from '@/stores/error'
+const errorStore = useErrorStore()
 
-const { activeError } = storeToRefs(useErrorStore())
+onErrorCaptured((error) => {
+  errorStore.setError({ error })
+})
 </script>
 
 <template>
   <AuthLayout>
-    <AppErrorPage v-if="activeError" />
+    <AppErrorPage v-if="errorStore.activeError" />
+
     <RouterView v-else v-slot="{ Component, route }">
       <Suspense v-if="Component" :timeout="0">
         <Component :is="Component" :key="route.name"></Component>
