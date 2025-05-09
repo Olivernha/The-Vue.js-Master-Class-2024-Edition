eebf0a450687f48ed65337aa05db408bcbf96f9e
Nay Htet Aung <nayha@tuaspower.com.sg>
2025-05-09 13:30:12 +0800

    feat: error store

--- src/pages/[...catchAll].vue
+++ src/pages/[...catchAll].vue
@@ -1,12 +1,9 @@
 <script setup lang="ts">
+
+
+useErrorStore().setError()
 </script>
+
 <template>
-  <div>
-    <h1>Not Found</h1>
-    <p>
-      The page you are looking for is not found. If you are the developer of this
-      site, ensure that the route is correctly registered in the
-      <code>router/index.ts</code> file.
-    </p>
-  </div>
+  <div></div>
 </template>
